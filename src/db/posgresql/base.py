0dcd5429f0cf4558a01d578ef0b44517e645a387
ronihdzz <ronaldo.runing_@hotmail.com>
2025-05-11 14:53:56 -0600

    feat: add posgresql settings with use example

---
+++ src/db/posgresql/base.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, func
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declared_attr
+import uuid
+from shared.utils_dates import get_app_current_time
+
+Base = declarative_base()
+
+
+class BaseModel:
+    @declared_attr
+    def id(cls):
+        return Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime, default=get_app_current_time)
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(DateTime, default=get_app_current_time, onupdate=get_app_current_time)
+
+    @declared_attr
+    def deleted_at(cls):
+        return Column(DateTime, nullable=True)
